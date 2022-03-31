ee4fcd5fdf88c4ce73de41853ba0af28fda980f4
unknown <rodrigo.kloeckner@gmail.com>
2022-03-31 15:52:04 -0300

    navigation - ok

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,74 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy_data";
+import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/results-title";
+import { Fragment } from "react";
+import Button from "../../components/ui/Button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEvents() {
-  return <div>Filtered Events</div>;
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2020 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show all Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
 }
 
 export default FilteredEvents;
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,20 @@ import { getFeaturedEvents } from "../dummy_data";
 import EventList from "../components/events/EventList";
 import EventsSearch from "../components/events/EventsSearch";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 function HomePage() {
   const featuredeEvents = getFeaturedEvents();
+  const router = useRouter();
 
-  console.log(featuredeEvents);
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={featuredeEvents} />
     </Fragment>
   );
